4c5898f6d20073debaf109760e0972691ed65051
Adriath <adrianarjonabravo@gmail.com>
2023-01-12 20:40:54 +0100

    Realizado el esqueleto de la clase Personaje.

--- src/modeladopersonaje/Personaje.java
+++ src/modeladopersonaje/Personaje.java
@@ -49,14 +49,69 @@ public class Personaje {
         numeroPersonajes++ ;
     }
     
-//    public Personaje(String nombre, byte inteligencia, byte fuerza) throws ExcepcionPersonaje{
-//        this.nombre = nombre ;
-//        this.vida = MAX_VIDA ;
-//        this.setInteligencia (inteligencia) ;
-//        this.setFuerza(fuerza) ;
-//        
-//        numeroPersonajes** ;
-//    }
+    public Personaje(String nombre, byte inteligencia, byte fuerza) throws ExcepcionPersonaje{
+        this.nombre = nombre ;
+        this.vida = MAX_VIDA ;
+        this.setInteligencia (inteligencia) ; // Lanza excepción si el valor no está permitido.
+        this.setFuerza(fuerza) ; // Lanza excepción si el valor no está permitido.
+        
+        numeroPersonajes++ ;
+    }
     
+    // --------------- MÉTODOS -----------------
     
+        // GETTERS & SETTERS
+
+            // Nombre
+    
+        public String getNombre() {
+            return nombre;
+        }
+
+        public void setNombre(String nombre) {
+            this.nombre = nombre;
+        }
+
+            // Inteligencia
+        
+        public byte getInteligencia() {
+            return inteligencia;
+        }
+
+        public void setInteligencia(byte inteligencia) throws ExcepcionPersonaje{
+            if ((inteligencia > MIN_INTELIGENCIA) && (inteligencia < MAX_INTELIGENCIA)) 
+            {
+                this.inteligencia = inteligencia ;
+            }
+            else
+            {
+                throw new ExcepcionPersonaje("\nEl valor no está permitido. Debe ser mayor que " + MIN_INTELIGENCIA + 
+                        " y menor que " + MAX_INTELIGENCIA + "\n") ;
+            }
+        }
+
+            // Fuerza
+        
+        public byte getFuerza() {
+            return fuerza;
+        }
+
+        public void setFuerza(byte fuerza) throws ExcepcionPersonaje{
+            if ((fuerza > MIN_FUERZA) && (fuerza < MAX_FUERZA)) 
+            {
+                this.fuerza = fuerza ;
+            }
+            else
+            {
+                throw new ExcepcionPersonaje("\nEl valor no está permitido. Debe ser mayor que " + MIN_FUERZA + 
+                        " y menor que " + MAX_FUERZA + "\n") ;
+            }
+        }
+
+    @Override
+    public String toString() {
+        return "Personaje{" + "nombre=" + nombre + ", vida=" + vida + ", inteligencia=" + inteligencia + ", fuerza=" + fuerza + '}';
+    }
+ 
+        
 }

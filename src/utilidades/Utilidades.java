66ac2d08302c686b47f3fea8f2e7bf698ff2740e
Adriath <adrianarjonabravo@gmail.com>
2023-01-12 17:19:35 +0100

    Commit inicial.

---
+++ src/utilidades/Utilidades.java
@@ -0,0 +1,16 @@
+
+package utilidades;
+
+/**
+ * Clase dedicada a métodos que ayuden al desarrollo de la tarea.
+ * Ejemplo: un método para leer por pantalla una cadena de caracteres.
+ * 
+ * @author Adrián Arjona
+ * @version 1.1 Enero 2023
+ */
+public class Utilidades {
+    
+    /* Francisco Adrián Arjona Bravo
+        UNIDAD 5: desarrollo de clases.
+    */
+}
